e9f793050adf30c82e15a905bdd2c1e6d399850f
RupeshReddyAleti <rupeshreddy588@gmail.com>
2023-08-14 15:04:50 +0530

    first

---
+++ DemoAmdocsB2Spring/src/main/java/com/demo/controller/DemoCont.java
@@ -0,0 +1,12 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoCont {
+	@RequestMapping("/get")
+	public String show() {
+		return "Welcome to amdocs";
+	}
+}
